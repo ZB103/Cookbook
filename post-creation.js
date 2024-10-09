d76630ba3b9cbd96d170f75ee7ee38725f354069
Con-Co-Industries <71508099+Con-Co-Industries@users.noreply.github.com>
2024-10-08 20:31:04 -0500

    Added data verification
    Updated the html and js for the post creation webpage to add data verification. I made sure the user can't exceed character limits, that they can't add empty values, that they can't upload giant pictures, and more. I also uploaded a 5mb+ pic to the github so that people who clone it to download will have something to test with.

--- post-creation.js
+++ post-creation.js
@@ -1,31 +1,92 @@
 // JS page written by Connor Ettinger
+
+// Function to check and make sure that a file is not too large
+function checkFileSize(){
+  // get the thing that was uploaded
+  let upload = document.getElementById("pic");
+  const MAX_FILE_SIZE = 5242880; // according to stack overflow, 1MB = 1,048,576, so 5MB = 5,242,880
+  // check the size of the file
+  if(upload.files[0].size > MAX_FILE_SIZE){
+    alert("Your file is too large. The max file size is 5MB.");
+    upload.value = "";
+  }
+  else{console.log(upload.files[0].size);}
+}
+
+// Function to check and make sure that a user isn't exceeding the character limit
+function limitCharLen(id,maxLen){
+  let element = document.getElementById(id);
+  if(element.value.length >= maxLen){
+    alert("The max character length for this field is " + maxLen +".");
+  }
+}
+
 // Function to dynamically add a new ingredient field
 function addIngredient(){
   // first get text from the input
   let ingredientInput = document.getElementById("ingredientInput").value;
   console.log(ingredientInput);
-  // second create a new list element
+  // second make sure the user doesn't try to submit an empty tag
+  if(ingredientInput.trim().length < 1 || ingredientInput === null){
+    alert("Please enter text in the ingredient");
+    return; // exit the funtion
+  }
+  // third make sure someone doesn't implement multiple of the same ingredient
+  // NOTE: I don't think there's really any way to prevent someone from putting in multiple of the same ingredient but just changing the measurement amount. I would love to be proven wrong though.
+  const MAX_INGREDIENTS = 20;
+  let prevIngredients = document.getElementById("ingredientList").children;
+  for(let i = 0; i < prevIngredients.length; i++){
+    if(prevIngredients[i].textContent === ingredientInput){
+      alert("Please don't enter duplicate ingredients");
+      return;
+    } else if(i >= (MAX_INGREDIENTS - 1)){
+      alert("Ingredient cap reached. A recipe can't have more than 20 ingredients.");
+      return;
+    }
+  }
+  // fourth create a new list element
   let newIngredient = document.createElement("li");
   newIngredient.textContent = ingredientInput;
-  // third add that list element to the ul
+  // finally add that list element to the ul
   document.getElementById("ingredientList").appendChild(newIngredient);
 }
 
 // function to dynamically add a tag to the page
 function addTag(){
-  // first get text from the input
+  // first make sure we haven't hit the tag limit
+  const MAX_TAGS = 15
+  let tagNum = document.getElementById("numOfTags");
+  if(Number(tagNum.value) >= MAX_TAGS){
+    alert("Tag cap reached. Your post can't have more than 15 tags.");
+    return;
+  }
+  // second get text from the input
   let tagInput = document.getElementById("tagInput");
   console.log(tagInput.value);
-  // second check if it starts with a #, and if not add it
+  // third check and make sure the tag text is acceptable
+  tagInput.value = tagInput.value.replaceAll(" ", ""); // remove spaces
+  // make sure the user doesn't try to submit an empty tag
+  if(tagInput.value.length < 1){
+    alert("Please enter text in the tag");
+    return;
+  }
+  // make sure all tags start with "#"
   if(!tagInput.value.startsWith("#")){
     tagInput.value = "#".concat(tagInput.value);
   }
-  // next add to current tags
+
+  // now that the tag has been verified, let's start the process of adding the tag to the field
   let tagField = document.getElementById("tagField"); // tagField is where the tags are displayed to the user. Doesn't exist until the user adds a tag
   
   // check if the tagField exists. if it doesn't, it will return null, which is evaluated as false in js
+  // so we'll create it
   if(tagField){
-    // if it exists, add the new tag the user put in to the tag field
+    // if it exists, make sure the tag isn't already there
+    if(tagField.value.includes(tagInput.value)){
+      alert("Please don't enter duplicate tags");
+      return;
+    }
+    // then add the tag to the input field
     let tagText = tagField.value + ", " + tagInput.value;
     tagField.setAttribute("value", tagText);
   } else {
@@ -49,4 +110,6 @@ function addTag(){
     // add the new tag the user put in to the tag field
     tagField.setAttribute("value", tagInput.value);
   }
+  // finally, update the number of tags
+  tagNum.value = Number(tagNum.value) + 1;
 }
