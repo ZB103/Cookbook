37d35b5a37dfe42a8c660f313383c4e586a873b2
Ani Dal <zbaker985@gmail.com>
2024-11-04 16:59:09 -0600

    GHAHHAHAHH

---
+++ static/bookmarkPage.js
@@ -0,0 +1,14 @@
+function getBookmarks() {
+    let p1 = document.getElementById("post1");
+    p1.textContent = "Fresh Fish";
+
+    let p2 = document.getElementById("post2");
+    p2.textContent = "Fresh Ifsh";
+
+    let p3 = document.getElementById("post3");
+    p3.textContent = "Fresh Shift";
+}
+
+function removeFromBookmark() {
+    alert("removeFromBookmark called");
+}
